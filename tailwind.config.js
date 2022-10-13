5f08a106d2b9dba9dc7511ad430e403806f8faf5
LourencoLegend <lourencolegend@yahoo.com>
2022-10-13 17:17:41 -0600

    extra files deleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  Footer
-  
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
