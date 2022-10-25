fcc5153cab86b3678a474334357eabeaca0e0fc2
RhodaMabs <mab21004@byui.edu>
2022-10-25 22:04:16 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,13 @@
-export function renderWithTemplate(template) {
-    let clone = template.content.cloneNode(true);
-    if (callback) {
-      clone = callback(clone, data);
-    }
-    parent.appendChild(clone);
-  }
-  
-  export async function loadTemplate(path) {
-    const html = await fetch(path).then(convertToText);
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template;
-  }
+
   
   // load the header and footer
-  export async function loadHeaderFooter() {
-    const header = await loadTemplate('../partials/header.html');
-    const footer = await loadTemplate('../partials/footer.html');
+   function loadHeaderFooter(header,footer) {
+    const header = '../partials/header.html';
+    const footer = '../partials/footer.html';
     const headerElement = document.getElementById('main-header');
     const footerElement = document.getElementById('main-footer');
-    renderWithTemplate(header, headerElement);
-    renderWithTemplate(footer, footerElement);
+    headerElement.innerText= header;
+    footerElement.innerText= footer;
   }
   
+  loadHeaderFooter(header,footer);
