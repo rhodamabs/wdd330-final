1b8569e2c90a1640c342c4cb2c15398c27438a3a
RhodaMabs <mab21004@byui.edu>
2022-10-27 03:31:48 +0200

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
+import { loadHeaderFooter } from './utils.js';
 
-  
-  // load the header and footer
-   function loadHeaderFooter(header,footer) {
-    const header = '../partials/header.html';
-    const footer = '../partials/footer.html';
-    const headerElement = document.getElementById('main-header');
-    const footerElement = document.getElementById('main-footer');
-    headerElement.innerText= header;
-    footerElement.innerText= footer;
-  }
-  
-  loadHeaderFooter(header,footer);
+loadHeaderFooter();
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,7 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+const now = new Date();
 const searchBox = document.querySelector('.search-box');
 searchBox.addEventListener('keypress', setQuery);
   
@@ -14,27 +15,37 @@ function setQuery(evt) {
   
 function getResults (query) {
   fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-    .then(weather => {
-      return weather.json();
+    .then(data => {
+      return data.json();
     }).then(displayResults);
 }
   
-function displayResults (weather) {
+function displayResults (data) {
   let city = document.querySelector('.location .city');
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
+  city.innerText = `${data.name}, ${data.sys.country}`;
 
-  let now = new Date();
   let date = document.querySelector('.location .date');
   date.innerText = dateBuilder(now);
   
   let temp = document.querySelector('.current .temp');
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
+  temp.innerHTML = `${Math.round(data.main.temp)}<span>°F</span>`;
 
   let weather_el = document.querySelector('.current .weather');
-  weather_el.innerText = weather.weather[0].main;
+  weather_el.innerText = data.weather[0].main;
  
   let hiLow = document.querySelector('.hi-low');
-  hiLow.innerText = `${Math.round(weather.main.temp_max)}°F / ${Math.round(weather.main.temp_min)}°F`;
+  hiLow.innerText = `${Math.round(data.main.temp_max)}°F / ${Math.round(data.main.temp_min)}°F`;
+
+  // Banner
+  if (data.weather[0].main === 'Rain' || data.weather[0].main === 'Snow') {
+    document.getElementById("banner").style.display = "block";
+  }
+
+  const close = document.querySelector("#close");
+
+  close.addEventListener("click", () => {
+    banner.style.display = "none";
+  });
 }
   
 function dateBuilder (d) {
@@ -48,21 +59,44 @@ function dateBuilder (d) {
   return `${day} ${month} ${date} `;
 }
 
+
 // 5 Day DropDown
-
-
-
-// Banner
-const now = new Date();
-if (now.getDay() === 1 || now.getDay() === 2) {
-  document.getElementById("banner").style.display = "block";
+function getFiveDayResults () {
+  let query = document.getElementById("search-box").value;
+  fetch(`${api.base}forecast?q=${query}&cnt=6&exclude=current,minutely,hourly&units=imperial&APPID=${api.key}`)
+    .then(data2 => {
+      return data2.json();
+}).then(displayFiveDayResults);
 }
 
-const close = document.querySelector("#close");
+function displayFiveDayResults(data2){
+  let dailyForecast = data2.list;
+  document.getElementById("fiveDayForecast").innerHTML = "";
+  for (x = 1; x < dailyForecast.length; x++) {
+    let day = dailyForecast[x];
+    let today = new Date().getDay() + x;
+    today = today % 7;
+    let dayOfWeek = getDayOfWeek(today);
+    let icon = day.weather[0].icon;
+    let highTemp = day.main.temp_max;
+    let lowTemp = day.main.temp_min;
+    displayWeatherDay(dayOfWeek,icon, highTemp, lowTemp)
+  }
+}
+let getDayOfWeek = function(dayNum){
+  const weekDay = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-close.addEventListener("click", () => {
-    banner.style.display = "none";
-});
+  return (weekDay[dayNum]);
+}
+
+let displayWeatherDay = function(dayOfWeek, icon, highTemp, lowTemp) {
+  let out = "<span class='day'>" + dayOfWeek + "</span>";
+  out += "<div class='weatherDay'><img src='http://openweathermap.org/img/wn/" + icon + "@2x.png'>";
+  out += "<p>" + Math.round(highTemp) + "°F / " + Math.round(lowTemp) + "°F</p>";
+  out += "<hr></hr>";
+  document.getElementById("fiveDayForecast").innerHTML += out;
+}
+
 
 // Dark Theme
 var icon = document.getElementById("icon");
